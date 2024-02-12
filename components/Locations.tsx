5c2322ce590d50629db20d7d9555ff6bcea55683
JoaoF17 <joao_fernandess17@hotmail.com>
2024-02-12 21:06:07 +0700

    location updated

--- components/Locations.tsx
+++ components/Locations.tsx
@@ -3,21 +3,21 @@ import React from 'react'
 
 const Locations = () => {
   return (
-    <section className='border-2 mt-4' id='locations'>
-      <div className="p-8 md:w-2/3">
-        <h1 className="text-4xl font-bold mb-2">Come Have Fun With Us</h1>
-        <p className="mb-4">
+    <section className='mt-4' id='locations'>
+      <div className="text-center p-8 md: mx-auto">
+        <h1 className="text-4xl font-bold mb-2 text-center">Come Have Fun With Us</h1>
+        <p className="mb-4 text-center">
           Brand new pitches with state-of-the-art fitness equipment, all conveniently located near you.
         </p>
-        <div className='flex flex-row gap-10'>
-          <div className="rounded-xl bg-yellow-50 text-white flex flex-col justify-center items-center p-8 md:w-1/3">
+        <div className='flex flex-col md:flex-row justify-center items-center gap-5 md:gap-5'>
+          <div className="rounded-xl bg-yellow-50 text-black flex flex-col justify-center items-center p-8 w-full md:w-1/3" style={{ maxWidth: '100%', height: '30vw' }}>
             <h2 className="text-3xl font-bold mb-4">Kicck Arena</h2>
             <button className="bg-white text-yellow-50 font-semibold py-2 px-4 rounded">
               View Location
             </button>
           </div>
-          <div className="flex justify-between items-center">
-            <Image src="/kicck-arena.jpeg" alt='kicck arena' className='rounded-xl' width="330" height="150"/>
+          <div className="relative w-full md:w-1/3" style={{ maxWidth: '100%', height: '30vw', minHeight: '250px' }}>
+            <Image src="/kicck-arena.jpeg" alt='kicck arena' className='rounded-xl' layout='fill' objectFit='cover'/>
           </div>
         </div>
       </div>
